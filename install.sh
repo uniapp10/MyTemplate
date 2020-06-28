d8120255a0f5517a917174db5e244e11601423a8
zhudongdong <zhudongdong@58.com>
2020-06-28 19:42:48 +0800

    调整好OC和Swift的工程模板

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ mkdir -p /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/
 mkdir -p /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project\ Templates/iOS/58.xctemplate
 
 cp -R ./FileTemplate/* /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/File\ Templates/58/
-cp -R ./ProjectTemplate/* /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project\ Templates/iOS/58.xctemplate
+cp -R ./ProjectTemplate/58.xctemplate/* /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project\ Templates/iOS/58.xctemplate
